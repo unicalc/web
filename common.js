010c0028e239948de531673e10321316f525db1e
Mark Reiche <porphyr@gmx.de>
2017-02-11 18:56:45 +0100

    - fixed rounding - added pictures of new units

--- common.js
+++ common.js
@@ -67,6 +67,7 @@ function pvalue(attacker, attackerTerrain, attackerBonus, defender, defenderTerr
 	var pierce = attacker.armorPiercing[defender.class];
 	if (pierce) {
 		Dd *= 1 - pierce;
+		Dd = Math.round(Dd);
 	}
 	var p1 = Aa ? 0.5 + 0.05 * (Aa-Dd) : 0;
 	return Math.min(Math.max(p1, 0), 1);
